0bf581e9a2d27ff3497092135be0ad796e67d9e4
Luiz <guilhermegomes110106@gmail.com>
2025-02-24 18:14:35 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao.py/aula2_questao1.py
@@ -0,0 +1,7 @@
+n1 = 5
+n2 = 2
+print(type(n1))
+print(type(n2))
+n3 = 5 / 2
+print(n3)
+print(type(n3))
---
+++ Modulo2/aula2_questao.py/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "o resultado é"
+n1 = 10
+n2 = 3.5
+n3 = n1 + n2
+print(texto)
+print(type(texto))
+print(n1)
+print(type(n1))
+print(n2)
+print(type(n2))
+print(n3)
+print(type(n3))
