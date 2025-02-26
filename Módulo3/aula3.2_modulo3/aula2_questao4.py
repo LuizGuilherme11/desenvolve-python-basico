0dc73e4a17c446589e5f5c0bfd96e90ae4172f91
Luiz <guilhermegomes110106@gmail.com>
2025-02-26 02:45:59 -0300

    Delete Módulo3/aula3.2_modulo3 directory

--- Módulo3/aula3.2_modulo3/aula2_questao4.py
+++
@@ -1,8 +0,0 @@
-#entrada
-classe = input("Escolha a classe do personagem (guerreiro, mago ou arqueiro): ").strip().lower()
-forca = int(input("Digite os pontos de Força: "))
-magia = int(input("Digite os pontos de Magia: "))
-#processamento e saída
-print((classe == "guerreiro" and forca >= 15 and magia <= 10) or
-      (classe == "mago" and forca <= 10 and magia >= 15) or
-      (classe == "arqueiro" and 5 < forca <= 15 and 5 < magia <= 15))
