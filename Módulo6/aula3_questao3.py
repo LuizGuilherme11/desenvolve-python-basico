81a56d3f248e2a435bc7bde27e98e8d891cd732a
Luiz <guilhermegomes110106@gmail.com>
2025-04-09 02:36:35 -0300

    aula3_questao3.py

--- Módulo6/aula3_questao3.py
+++ Módulo6/aula3_questao3.py
@@ -1,10 +1,12 @@
 import random # importa a função random
-lista1 = [random.randint(0,50) for i in range(20)] #gera 20 números aleatórios na lista 1
+lista1 = [random.randint(0,50) for i in range(20)]# gera 20 números aleatórios na lista 1
 lista2 = [random.randint(0,50) for i in range(20)]# gera 20 números aleatórios na lista 2
-interseccao = list(set(lista1) & set(lista2)) # encontra as intersecções entre as 2 listas
-print(lista1)
-print(lista2)
-interseccao_ordenada = interseccao.sort # gera a lista de interseccao ordenada
-print(interseccao_ordenada)
+print(sorted(lista1))
+print(sorted(lista2))
+interseccao = []
+for elemento in lista1: # encontra as intersecções entre as 2 listas sem repetição
+    if elemento in lista2 and interseccao not in interseccao:
+        interseccao.append(elemento)
+interseccao.sort() # gera a lista de interseccao ordenada
 for num in interseccao: # mostra a quantidade que cada elemento aparece em cada lista
-    print(f"{num}: lista 1 = {lista1.count(num)}", f"{num}: lista 2 = {lista2.count(num)}")
+    print(f"{num}: lista 1 = {lista1.count(num)}"" e", f"{num}: lista 2 = {lista2.count(num)}")
